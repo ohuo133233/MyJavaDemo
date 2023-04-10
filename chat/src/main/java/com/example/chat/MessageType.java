7aa5f5db836cf1013b7c0438f1dc03616646e0b8
ohuo133233 <1441289824@qq.com>
2023-04-10 22:42:32 +0800

    合并丢失的代码

---
+++ chat/src/main/java/com/example/chat/MessageType.java
@@ -0,0 +1,11 @@
+package com.example.chat;
+
+public class MessageType {
+    // 上线消息
+    public static final int ON_LINE = 1;
+    // 系统消息
+    public static final int SYSTEM_MESSAGE = 2;
+    // 普通消息
+    public static final int MESSAGE = 3;
+
+}
