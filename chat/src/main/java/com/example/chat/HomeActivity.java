d22d4798c9c8be01a52309ba1c81427581f56232
ohuo133233 <1441289824@qq.com>
2023-04-18 00:06:18 +0800

    完善UI

--- chat/src/main/java/com/example/chat/HomeActivity.java
+++ chat/src/main/java/com/example/chat/HomeActivity.java
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.example.chat.base.CommonFragmentAdapter;
 import com.example.chat.chat.ChatListFragment;
 import com.example.chat.friends.FriendsFragment;
+import com.example.chat.more.MoreFragment;
+import com.example.chat.my.MyFragment;
 
 import java.util.ArrayList;
 
@@ -27,14 +31,50 @@ public class HomeActivity extends AppCompatActivity {
     private void initView() {
         ChatListFragment chatFragment = new ChatListFragment();
         FriendsFragment friendsFragment = new FriendsFragment();
+        MoreFragment moreFragment = new MoreFragment();
+        MyFragment myFragment = new MyFragment();
 
         ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
 
         fragmentArrayList.add(chatFragment);
         fragmentArrayList.add(friendsFragment);
+        fragmentArrayList.add(moreFragment);
+        fragmentArrayList.add(myFragment);
         CommonFragmentAdapter commonFragmentAdapter = new CommonFragmentAdapter(this, fragmentArrayList);
 
         mViewPager2 = findViewById(R.id.view_page);
         mViewPager2.setAdapter(commonFragmentAdapter);
+
+
+        Button chat = findViewById(R.id.chat);
+        Button friends = findViewById(R.id.friends);
+        Button more = findViewById(R.id.more);
+        Button my = findViewById(R.id.my);
+
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager2.setCurrentItem(0);
+            }
+        });
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager2.setCurrentItem(1);
+            }
+        });
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager2.setCurrentItem(2);
+            }
+        });
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager2.setCurrentItem(3);
+            }
+        });
     }
 }
--- chat/src/main/java/com/example/chat/friends/FriendsFragment.java
+++ chat/src/main/java/com/example/chat/friends/FriendsFragment.java
@@ -2,13 +2,21 @@ package com.example.chat.friends;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chat.R;
+import com.example.chat.base.CommonRecyclerViewAdapter;
+import com.example.chat.base.CommonRecyclerViewAdapterBackCall;
+import com.example.chat.base.CommonRecyclerViewHolder;
+
+import java.util.ArrayList;
 
 
 public class FriendsFragment extends Fragment {
@@ -16,7 +24,36 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View layout = inflater.inflate(R.layout.fragment_friends, container, false);
+        initView(layout);
+        return layout;
+    }
+
+    private void initView(View layout) {
+
+        RecyclerView recyclerView = layout.findViewById(R.id.friends_list);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("1");
+        strings.add("1");
+        strings.add("1");
+        strings.add("1");
+
+
+        CommonRecyclerViewAdapter<String> recyclerViewAdapter = new CommonRecyclerViewAdapter.Build<String>()
+                .setContext(requireContext())
+                .setLayoutId(R.layout.friends_item)
+                .setDataList(strings)
+                .build();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        recyclerViewAdapter.setCommonRecyclerViewAdapterBackCall(new CommonRecyclerViewAdapterBackCall() {
+            @Override
+            public void onBindViewHolder(@NonNull CommonRecyclerViewHolder holder, int position) {
+
+            }
+        });
+
     }
 }
