f4dbf05882b5897f1fa44704d5f7fa0b32e17a87
ohuo133233 <1441289824@qq.com>
2023-07-03 22:48:51 +0800

    更新手势代码

--- ui/src/main/java/com/example/ui/gesture/GestureActivity.java
+++ ui/src/main/java/com/example/ui/gesture/GestureActivity.java
@@ -11,6 +11,8 @@ import com.example.ui.R;
 
 public class GestureActivity extends AppCompatActivity {
     private ImageView mImageView;
+    private float lastX;
+    private float lastY;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,24 +22,40 @@ public class GestureActivity extends AppCompatActivity {
 
 
     }
-    private float moveX;
-    private float moveY;
+
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent( MotionEvent event) {
+        float currentX = event.getRawX();
+        float currentY = event.getRawY();
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                moveX = event.getX();
-                moveY = event.getY();
+                lastX = currentX;
+                lastY = currentY;
                 break;
+
             case MotionEvent.ACTION_MOVE:
-                mImageView.setTranslationX(event.getX() + (event.getX() - moveX));
-                mImageView.setTranslationY(event.getY() + (event.getY() - moveY));
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
-            case MotionEvent.ACTION_CANCEL:
+                float deltaX = currentX - lastX;
+                float deltaY = currentY - lastY;
+
+
+
+                // 计算新的位置
+                float newX = mImageView.getX() + deltaX;
+                float newY = mImageView.getY() + deltaY;
+
+                // 更新视图的位置
+                mImageView.setX(newX);
+                mImageView.setY(newY);
+
+                lastX = currentX;
+                lastY = currentY;
                 break;
         }
+
+
         return true;
     }
+
 }
